b01e7367221d83997f26e7f74c7f7673473e5f84
Aelanar <stabaurelien@gmail.com>
2017-02-21 17:38:33 +0100

    Création du comportement de patrouille Associé pour le moment à la classe enemy création d'une classe utilitaire listPos pour recupérer la liste des positions des enfants de l'objets, enemy appelera cette fonctions pour récupérer les différentes positions qu'il doit parcourir

---
+++ Navi/Assets/Assets/Script/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    /// <summary>
+    /// Implémentation du comportement patrol :
+    /// On recupère la liste de positions des objets enfants et on va successivement à chacune de ces positions
+    /// </summary>
+    
+    private Transform[] transformList;
+    private Vector3[] positionsList;
+    private int pos=0;
+    private int maxPos;
+    public float speed=1;
+
+	// mise en place des positions
+	void Start () {
+        transformList = this.gameObject.GetComponentInChildren<ListPos>().GetListPos();
+        maxPos = transformList.Length;
+        positionsList = new Vector3[maxPos];
+        for(int i= 0; i < maxPos;i++)
+        {
+            positionsList[i] = transformList[i].position;
+        }
+    }
+
+
+	// Déplcament vers la position target
+	void Update () {
+        float step = speed * Time.deltaTime;
+        Vector3 target = positionsList[pos];
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+        if ((transform.position - target).sqrMagnitude < step)
+        {
+            pos = (pos+1)%maxPos ;
+        }
+	}
+}
---
+++ Navi/Assets/Assets/Script/ListPos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListPos : MonoBehaviour
+{/// <summary>
+/// Classe qui recupère la liste des positions de ses enfants
+/// </summary>
+    public Transform[] GetListPos()
+    {
+        Transform[]  positions = this.GetComponentsInChildren<Transform>();
+        return positions;
+    }
+}
